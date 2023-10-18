847438157b11b13c7c6a09ac4841a8c3adf661d1
Juan_C_Hernandez <33169379+JuanC-Hernandez@users.noreply.github.com>
2023-10-17 19:41:10 -0700

    update note 1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,16 +20,75 @@ namespace Prog_122_L9_ListBoxes_ComboBoxes
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
+
+        // New List of students favorite colors
+        List<string> StudentsFavoriteColors = new List<string>();
+        List<string> studenNames = new List<string>();  
         public MainWindow()
         {
             InitializeComponent();
+
+            // .add()
+
+            //rtbDisplay.Text = StudentsFavoriteColors[2];
+            //rtbDisplay.Text += "Carlos favorite color is white";
+            preload();
+
+            Display(StudentsFavoriteColors);
+            //Display(studenNames);
+
         }//MainWindow
+
+        public void preload()
+        {
+            StudentsFavoriteColors.Add("Hafsa - Black");
+            StudentsFavoriteColors.Add("Matt - Orange");
+            StudentsFavoriteColors.Add("Benel - Green");
+            StudentsFavoriteColors.Add("David - Pink");
+            StudentsFavoriteColors.Add("Kira - Purple");
+            StudentsFavoriteColors.Add("Juan - Light cornflower blue");
+            StudentsFavoriteColors.Add("Benel - Green");
+            //Display();
+            studenNames.Add("Marco");
+            studenNames.Add("Polo");
+            studenNames.Add("Paco");
+            studenNames.Add("Loco");
+        }
+        public void Display()
+        {
+            rtbDisplay.Text = "";
+            for (int i = 0; i < StudentsFavoriteColors.Count; i++)
+            {
+                rtbDisplay.Text += StudentsFavoriteColors[i] + "\n";
+            }
+
+        }
+
+        public void Display(List<string> list) 
+        {
+            rtbDisplay.Text = "";
+            for (int i = 0; i < list.Count; i++)
+            {
+                rtbDisplay.Text += list[i] + "\n";
+            }
+
+        }
 
         // A collection in programming is any 
         // data structure that holds multiple
         // variables of the same type
 
         // array or list
+
+        //Lists<type> ListName = new List<type>();
+        List<string> strings = new List<string>();  
+
+        // Add 
+        // Access
+        // Remove
+
     }
 }//Class
 //namespace
