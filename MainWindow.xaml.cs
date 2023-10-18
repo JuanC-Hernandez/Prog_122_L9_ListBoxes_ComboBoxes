d4edabda8e61a466efed9e608f200e64dc8b08a7
Juan_C_Hernandez <33169379+JuanC-Hernandez@users.noreply.github.com>
2023-10-17 18:23:12 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prog_122_L9_ListBoxes_ComboBoxes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }//MainWindow
+
+        // A collection in programming is any 
+        // data structure that holds multiple
+        // variables of the same type
+
+        // array or list
+    }
+}//Class
+//namespace
